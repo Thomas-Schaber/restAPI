f4bf7ac4fae05f6ec89610bd80cd69cbbcc05893
ThomasSchaber <thschaber@my.waketech.edu>
2021-10-24 14:57:39 -0400

    Cleaned up clutter in code

--- test_presidents_name.py
+++ test_presidents_name.py
@@ -3,13 +3,10 @@ import pytest
 
 presList = ["Washington", "Adams", "Jefferson", "Madison", "Monroe", "Adams", "Jackson", "Buren", "Harrison", "Tyler", "Polk", "Taylor", "Fillmore", "Grant", "Hayes", "Garfield", "Arthur", "Cleveland", "Harrison", "Cleveland", "McKinley", "Roosevelt", "Taft", "Wilson", "Harding", "Coolidge", "Hoover", "Roosevelt", "Truman", "Eisenhower", "Kennedy", "Johnson", "Nixon", "Ford", "Carter", "Reagan", "Bush", "Clinton", "Bush", "Obama", "Trump", "Biden"]
 url = 'https://api.duckduckgo.com'
-reponse = requests.get(url + '/?q=presidents of the united states,&format=json')
-data = reponse.json()
+response = requests.get(url + '/?q=presidents of the united states,&format=json')
+data = response.json()
 
-#print(len(data['RelatedTopics']))
-#print(data['RelatedTopics']['Text'])
-# for line in data['RelatedTopics']:
-#     print(line['Text'])
+
 @pytest.mark.parametrize("pres", presList)
 def test_presidents_list(pres):
     found = False
