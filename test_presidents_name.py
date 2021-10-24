ea4aaa7142cb2feaed8b9d47471f502b890efe5f
ThomasSchaber <thschaber@my.waketech.edu>
2021-10-24 14:54:14 -0400

    Added list of presidents last names and unit test to check if the names show up in the Duck Duck Go Query

--- test_presidents_name.py
+++ test_presidents_name.py
@@ -1,6 +1,22 @@
+import requests
 import pytest
 
+presList = ["Washington", "Adams", "Jefferson", "Madison", "Monroe", "Adams", "Jackson", "Buren", "Harrison", "Tyler", "Polk", "Taylor", "Fillmore", "Grant", "Hayes", "Garfield", "Arthur", "Cleveland", "Harrison", "Cleveland", "McKinley", "Roosevelt", "Taft", "Wilson", "Harding", "Coolidge", "Hoover", "Roosevelt", "Truman", "Eisenhower", "Kennedy", "Johnson", "Nixon", "Ford", "Carter", "Reagan", "Bush", "Clinton", "Bush", "Obama", "Trump", "Biden"]
+url = 'https://api.duckduckgo.com'
+reponse = requests.get(url + '/?q=presidents of the united states,&format=json')
+data = reponse.json()
 
-def test_presidents_list():
-
-    assert True
+#print(len(data['RelatedTopics']))
+#print(data['RelatedTopics']['Text'])
+# for line in data['RelatedTopics']:
+#     print(line['Text'])
+@pytest.mark.parametrize("pres", presList)
+def test_presidents_list(pres):
+    found = False
+    for line in data['RelatedTopics']:
+        if pres in line['Text']:
+            found = True
+    if found:
+        assert True
+    else:
+        assert False
